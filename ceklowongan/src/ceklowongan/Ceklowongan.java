e859f398649a1d681eb026777a9c12a51ac320b2
Nadia130201 <nadnadiaaa13@gmail.com>
2020-05-13 15:59:37 +0700

    tugasakhir
    nadia

---
+++ ceklowongan/src/ceklowongan/Ceklowongan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ceklowongan;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nadia
+ */
+public class Ceklowongan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("PROGRAM LOKER");
+        JOptionPane.showMessageDialog(null,"SELAMAT DATANG DALAM PROGRAM PENCARIAN LOWONGAN PEKERJAAN");
+        new proloker().setVisible(true);    }
+    
+}
